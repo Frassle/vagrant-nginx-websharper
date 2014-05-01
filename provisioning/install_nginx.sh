71829e6110e38596ddd9cbd7b8bc56d66491821b
Fraser Waters <fraser.waters@arm.com>
2014-05-01 15:52:27 +0100

    Trying to start mono-server on startup.

--- provisioning/install_nginx.sh
+++ provisioning/install_nginx.sh
@@ -3,12 +3,12 @@
 cp /vagrant/provisioning/nginx/sites-available/default /etc/nginx/sites-available/default
 cp /vagrant/provisioning/nginx/fastcgi_params /etc/nginx/fastcgi_params
 
-mkdir /etc/init.d/mono-fastcgi
-cp /vagrant/provisioning/localhost.webapp /etc/init.d/mono-fastcgi/localhost.webapp
+mkdir /etc/fastcgi-mono-server
+cp /vagrant/provisioning/localhost.webapp /etc/fastcgi-mono-server/localhost.webapp
+chmod -R a+r /etc/fastcgi-mono-server
 
 mkdir -p /var/log/mono
 touch /var/log/mono/fastcgi.log
+chmod -R a+rw /var/log/mono
 
-fastcgi-mono-server4 --appconfigdir /etc/init.d/mono-fastcgi /socket=tcp:127.0.0.1:9000 /logfile=/var/log/mono/fastcgi.log &
-
-service nginx restart
+cp /vagrant/provisioning/fastcgi-mono-server.conf /etc/init/fastcgi-mono-server.conf
